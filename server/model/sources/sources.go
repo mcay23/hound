311d977e6ded2028f1823cb9560ac9225331d3d3
Hound <houndmediaserver@proton.me>
2023-04-04 01:56:23 +0900

    add release year info to games view

--- server/model/sources/sources.go
+++ server/model/sources/sources.go
@@ -1,12 +1,5 @@
 package sources
 
-import "fmt"
-
 func InitializeSources() {
 	InitializeTMDB()
-	test, err := GetGameFromIDIGDB(121)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(test.Name)
 }
