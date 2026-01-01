9fd092683dfc1e268b6b77f02510cc25cc2395d0
Hound <houndmediaserver@proton.me>
2026-01-01 22:15:49 +0700

    fix aes encoding issue

--- server/model/crypto.go
+++ server/model/crypto.go
@@ -1,11 +1,15 @@
 package model
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/pbkdf2"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
-	"errors"
+	"io"
 	"os"
-
-	"github.com/golang-jwt/jwt/v4"
 )
 
 // to encode into JWT string
@@ -14,39 +18,83 @@ type StreamObjectFull struct {
 	StreamObject
 }
 
-/*
-Encodes a stream into a string using JWT
-*/
-func EncodeJsonStreamJWT(streamObject StreamObjectFull) (string, error) {
-	var jwtKey = []byte(os.Getenv("JWT_SECRET_KEY"))
-	// Marshal the struct into JSON
-	bytes, _ := json.Marshal(streamObject)
+func getAESKey(salt []byte) (*[]byte, error) {
+	key, err := pbkdf2.Key(sha256.New, os.Getenv("HOUND_SECRET"), salt, 4096, 16)
+	return &key, err
+}
 
-	// Create a token with the JSON stored under one claim (e.g. "data")
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"data": string(bytes),
-	})
-	return token.SignedString(jwtKey)
+/*
+Encodes a stream into a string using AES
+This also protects api keys in urls from being
+exposed if hound link is shared
+*/
+func EncodeJsonStreamAES(streamObject StreamObjectFull) (string, error) {
+	salt := make([]byte, 16)
+	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
+		return "", err
+	}
+	key, err := getAESKey(salt)
+	if err != nil {
+		return "", err
+	}
+	bytes, err := json.Marshal(streamObject)
+	if err != nil {
+		return "", err
+	}
+	block, err := aes.NewCipher(*key)
+	if err != nil {
+		return "", err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	io.ReadFull(rand.Reader, nonce)
+	ciphertext := gcm.Seal(nonce, nonce, bytes, nil)
+	final := append(salt, ciphertext...)
+
+	return base64.URLEncoding.EncodeToString(final), nil
 }
 
 /*
 Decode a string back into StreamObjectFull data
 */
-func DecodeJsonStreamJWT(tokenString string) (*StreamObjectFull, error) {
-	var jwtKey = []byte(os.Getenv("JWT_SECRET_KEY"))
-	parsed, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return jwtKey, nil
-	})
+func DecodeJsonStreamAES(encryptedText string) (*StreamObjectFull, error) {
+	fullCiphertext, err := base64.URLEncoding.DecodeString(encryptedText)
 	if err != nil {
 		return nil, err
 	}
-	if claims, ok := parsed.Claims.(jwt.MapClaims); ok && parsed.Valid {
-		var decoded StreamObjectFull
-		err := json.Unmarshal([]byte(claims["data"].(string)), &decoded)
-		if err != nil {
-			return nil, err
-		}
-		return &decoded, nil
+	if len(fullCiphertext) < 16 {
+		return nil, io.ErrUnexpectedEOF
 	}
-	return nil, errors.New("DecodeJsonStreamJWT(): could not parse JWT claims")
+	salt := fullCiphertext[:16]
+	ciphertext := fullCiphertext[16:]
+
+	key, err := getAESKey(salt)
+	if err != nil {
+		return nil, err
+	}
+	block, err := aes.NewCipher(*key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, io.ErrUnexpectedEOF
+	}
+	nonce, actualCiphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	bytes, err := gcm.Open(nil, nonce, actualCiphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+	var streamObject StreamObjectFull
+	if err := json.Unmarshal(bytes, &streamObject); err != nil {
+		return nil, err
+	}
+	return &streamObject, nil
 }
